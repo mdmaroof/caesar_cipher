f1016da076f027cc573c801bc5ba2a0f605c6c61
Mohd Maroof <mdmaroof@outlook.com>
2024-02-20 23:11:33 +0530

    test

--- index.js
+++ index.js
@@ -1,54 +1,48 @@
 const movingShift = (s, shift) => {
-    let encodedString = encodeShifting(s, shift);
+    let encoded = '';
+    let shiftIncrement = shift;
 
-    const partLength = Math.floor(encodedString.length / 5);
-    let remainingLength = encodedString.length;
+    for (let i = 0; i < s.length; i++) {
+        encoded += encodeShifting(s[i], shiftIncrement);
+        shiftIncrement++;
+    }
+
+    const partLengths = new Array(5).fill(Math.floor(encoded.length / 5));
+    for (let i = 0; i < encoded.length % 5; i++) {
+        partLengths[i]++;
+    }
 
     const parts = [];
-
+    let startIndex = 0;
     for (let i = 0; i < 5; i++) {
-        const currentLength = Math.min(remainingLength, partLength + (5 - i - 1));
-
-        parts.push(encodedString.slice(0, currentLength));
-
-        encodedString = encodedString.slice(currentLength);
-        remainingLength -= currentLength;
+        const part = encoded.substring(startIndex, startIndex + partLengths[i]);
+        parts.push(part);
+        startIndex += partLengths[i];
     }
 
     return parts;
 }
 
-const encodeShifting = (s, shift) => {
-    let encoded = "";
-    for (let i = 0; i < s.length; i++) {
-        const char = s[i];
-        if (isLetter(char)) {
-            const newShift = shift % 26;
-            const newChar = shiftChar(char, newShift);
-            encoded += newChar;
-            shift++;
-        } else {
-            encoded += char;
-        }
+const encodeShifting = (char, shift) => {
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+    const isUpperCase = char === char.toUpperCase();
+    const lowerChar = char.toLowerCase();
+    const index = (alphabet.indexOf(lowerChar) + shift) % alphabet.length;
+    let encodedChar;
+    if (isLetter(char)) {
+        encodedChar = alphabet[index];
+    } else {
+        console.log(encodedChar, char)
+        encodedChar = char;
     }
-    return encoded;
+
+    return isUpperCase ? encodedChar.toUpperCase() : encodedChar;
 }
+
 const isLetter = (char) => {
     return /[a-zA-Z]/.test(char);
 }
 
-const shiftChar = (char, shift) => {
-    const alphabetSize = 26;
-    const shiftedCode = (char.charCodeAt(0) - getAlphabetBase(char) + shift) % alphabetSize;
-
-    const newCode = getAlphabetBase(char) + (shiftedCode + alphabetSize) % alphabetSize;
-
-    return String.fromCharCode(newCode);
-}
-
-const getAlphabetBase = (char) => {
-    return char === char.toUpperCase() ? 65 : 97;
-}
 
 const s = "I should have known that you would have a perfect answer for me!!!";
 const shift = 1;
